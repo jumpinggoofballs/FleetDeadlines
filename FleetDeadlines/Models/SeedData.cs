50953ae6509b93bc76c56af53823a9de09085793
jumpinggoofballs <vlad.l.cadar@gmail.com>
2023-07-23 17:01:07 +0100

    Beautifying code in (some) Razor pages

--- FleetDeadlines/Models/SeedData.cs
+++ FleetDeadlines/Models/SeedData.cs
@@ -27,7 +27,8 @@ namespace FleetDeadlines.Models
 
                 // Seeding
                 List<string> testVRNs = new List<string>
-                    { "AA19AAA", "AA19EEE", "AA19PPP", "L2WPS", "AA19SRN", "AA19DSL", "AA19MOT", "AA19AMP"};
+                    // { "AA19AAA", "AA19EEE", "AA19PPP", "L2WPS", "AA19SRN", "AA19DSL", "AA19MOT", "AA19AMP"};
+                    { "AA19AMP", "AA19MOT", "AA19PPP" };
 
                 // TODO: Make these requests parallel
                 foreach (var vrn in testVRNs)
